aacff61df2fe8e737ab61e168e323ca3e54fc1e1
Xeveryeo <143580217+Xeveryeo@users.noreply.github.com>
2024-02-26 21:53:43 +0800

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,16 +14,16 @@ player.on('timeupdate', throttle(timeFunction, 1000));
 player
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
   .then(function (seconds) {
-
+    // seconds = the actual time that the player seeked to
   })
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-
+        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-
+        // some other error occurred
         break;
     }
   });
